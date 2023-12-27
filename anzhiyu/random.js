94a5a618d3b0b94f2ab9420acceb45fcb8151326
BJD on TX3 <2296500135@qq.com>
2023-12-27 16:29:56 +0800

    Site updated: 2023-12-27 16:29:56

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2023/12/27/5/","2023/12/27/4/","2023/12/26/2/","2023/12/26/1/","2023/12/27/3/"];function toRandomPost(){
+var posts=["2023/12/26/1/","2023/12/26/2/","2023/12/27/5/","2023/12/27/4/","2023/12/27/3/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
