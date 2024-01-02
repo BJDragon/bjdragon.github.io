918af251e7fee51df4406b9d3aca84fe41315e39
BJD on TX3 <2296500135@qq.com>
2024-01-02 16:40:06 +0800

    Site updated: 2024-01-02 16:40:06

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2023/12/26/1/","2023/12/27/3/","2023/12/26/2/","2023/12/27/pytorch/","2023/12/27/4/","2023/12/27/分类算法/","2023/12/27/5/"];function toRandomPost(){
+var posts=["2023/12/27/3/","2023/12/27/5/","2023/12/27/4/","2023/12/27/分类算法/","2023/12/26/1/","2023/12/26/python常用操作/","2023/12/27/pytorch/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
