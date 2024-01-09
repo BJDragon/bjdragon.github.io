95e1dd1859389933aa10ee646563d8c8a0fb2d74
BJD on MSI <2296500135@qq.com>
2024-01-09 23:25:35 +0800

    Site updated: 2024-01-09 23:25:32

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2023/12/27/5/","2023/12/27/Life/","2023/12/27/4/","2023/12/27/pytorch/","2023/12/27/分类算法/","2023/12/26/python常用操作/","2023/12/26/python绘图/"];function toRandomPost(){
+var posts=["2023/12/27/4/","2023/12/27/5/","2023/12/27/Life/","2023/12/26/python常用操作/","2023/12/26/python绘图/","2023/12/27/pytorch/","2023/12/27/分类算法/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
